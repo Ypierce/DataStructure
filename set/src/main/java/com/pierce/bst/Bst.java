c7b71855235ff53f2a60cc7b6b4abf7c0edd5f59
Ypierce <1976933130@qq.com>
2020-03-15 16:28:30 +0800

    集合set

--- set/src/main/java/com/pierce/bst/Bst.java
+++ set/src/main/java/com/pierce/bst/Bst.java
@@ -4,6 +4,10 @@ public class Bst {
     public static void main(String[] args) {
 
         System.out.print("testOne");
+        System.out.print("testOne");
+        System.out.print("testOne");
+        System.out.print("testOne");
+
     }
 
 }
